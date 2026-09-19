cda93b1a0b59989599ab4e2d729204c9cc9016fa
aztecdev <contactos@mnaztecdev.com>
2026-09-19 09:51:24 -0600

    constants for prod

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,7 @@
-export const ORDERS_API_URI = "http://localhost:9000/pedidos";
-export const ORDER_CREATE_URI = "http://localhost:9000/pedido";
-export const ORDER_PATCH_URI = "http://localhost:9000/modificar/pedido/";
-export const ORDER_DELETE_URI = "http://localhost:9000/borrar/pedido/";
-export const USERS_API_URI = "http://localhost:9000/usuarios";
+export const ORDERS_API_URI = "https://liverpool-backend.onrender.com/pedidos";
+export const ORDER_CREATE_URI = "https://liverpool-backend.onrender.com/pedido";
+export const ORDER_PATCH_URI =
+  "https://liverpool-backend.onrender.com/modificar/pedido/";
+export const ORDER_DELETE_URI =
+  "https://liverpool-backend.onrender.com/borrar/pedido/";
+export const USERS_API_URI = "https://liverpool-backend.onrender.com/usuarios";
